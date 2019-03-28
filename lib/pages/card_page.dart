2071be45febe2e52d1e5ae2580babff24ae65e7c
Ken Harris <kentheprogrammer@gmail.com>
2019-03-28 15:47:55 -0400

    Reformatting code files.

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -21,111 +21,114 @@ class CardPageState extends State<CardPage> {
     bloc.dispose();
     super.dispose();
   }
-  
+
   @override
-  Widget build (BuildContext ctxt) {
+  Widget build(BuildContext ctxt) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Card Details"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Text("${widget._card.name}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, decoration: TextDecoration.underline))
-            ),
-            Padding(
-              padding:EdgeInsets.all(10),
-              child: Row(
+        appBar: AppBar(
+          title: Text("Card Details"),
+        ),
+        body: Center(
+            child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("Cost: ", style: TextStyle(fontSize: 18)),
-                  CardCost(
-                    coins: widget._card.coins, 
-                    potions: widget._card.potions,
-                    debt: widget._card.debt,
-                    compositePile: widget._card.isCompositePile,
-                    fontSize: 18,
-                    iconSize: 10
-                  )
-                ]
-              )
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: CardText(
-                top: widget._card.topText,
-                bottom: widget._card.bottomText,
-                fontSize: detailFontSize,
-              )
-            ),
-            StreamBuilder<CardDetailsState>(
-              stream: bloc.cardDetailsStream,
-              builder: (BuildContext context, AsyncSnapshot<CardDetailsState> snapshot) {
-                if (snapshot.hasData && (snapshot.data.constituentCards != null && snapshot.data.constituentCards.length > 0)) {
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "This card pile is composed of the following cards:",
-                          textAlign: TextAlign.center,
-                          style:TextStyle(fontSize: detailFontSize, fontWeight: FontWeight.bold, decoration: TextDecoration.underline)
-                        ),
-                        Text(
-                          "${snapshot.data.constituentCards.map((c) => c.name).join(", ")}", 
-                          textAlign: TextAlign.center,
-                          style:TextStyle(fontSize: detailFontSize)
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-            StreamBuilder<CardDetailsState>(
-              stream: bloc.cardDetailsStream,
-              builder: (BuildContext context, AsyncSnapshot<CardDetailsState> snapshot) {
-                if (snapshot.hasData && (snapshot.data.broughtCards != null && snapshot.data.broughtCards.length > 0)) {
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "This card pile brings the following cards:", 
-                          textAlign: TextAlign.center,
-                          style:TextStyle(fontSize: detailFontSize, fontWeight: FontWeight.bold, decoration: TextDecoration.underline)
-                        ),
-                        Text(
-                          "${snapshot.data.broughtCards.map((c) => c.name).join(", ")}", 
-                          textAlign: TextAlign.center,
-                          style:TextStyle(fontSize: detailFontSize)
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-          ]
-        )
-      )
-    );
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("${widget._card.name}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          decoration: TextDecoration.underline))),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Cost: ", style: TextStyle(fontSize: 18)),
+                        CardCost(
+                            coins: widget._card.coins,
+                            potions: widget._card.potions,
+                            debt: widget._card.debt,
+                            compositePile: widget._card.isCompositePile,
+                            fontSize: 18,
+                            iconSize: 10)
+                      ])),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: CardText(
+                    top: widget._card.topText,
+                    bottom: widget._card.bottomText,
+                    fontSize: detailFontSize,
+                  )),
+              StreamBuilder<CardDetailsState>(
+                stream: bloc.cardDetailsStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<CardDetailsState> snapshot) {
+                  if (snapshot.hasData &&
+                      (snapshot.data.constituentCards != null &&
+                          snapshot.data.constituentCards.length > 0)) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                              "This card pile is composed of the following cards:",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: detailFontSize,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline)),
+                          Text(
+                              "${snapshot.data.constituentCards.map((c) => c.name).join(", ")}",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: detailFontSize)),
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+              StreamBuilder<CardDetailsState>(
+                stream: bloc.cardDetailsStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<CardDetailsState> snapshot) {
+                  if (snapshot.hasData &&
+                      (snapshot.data.broughtCards != null &&
+                          snapshot.data.broughtCards.length > 0)) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          Text("This card pile brings the following cards:",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: detailFontSize,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline)),
+                          Text(
+                              "${snapshot.data.broughtCards.map((c) => c.name).join(", ")}",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: detailFontSize)),
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ])));
   }
 }
 
 class CardPage extends StatefulWidget {
   final DominionCard _card;
 
-  CardPage(this._card);  
+  CardPage(this._card);
 
   @override
   State<StatefulWidget> createState() => CardPageState();
--- lib/pages/kingdom_page.dart
+++ lib/pages/kingdom_page.dart
@@ -37,74 +37,68 @@ class KingdomPageState extends State<KingdomPage> {
   }
 
   void _respondToSwap(SwapState state) {
-    if (state.initialCard != null && state.swappedCard != null && _scaffold != null) {
+    if (state.initialCard != null &&
+        state.swappedCard != null &&
+        _scaffold != null) {
       if (state.wasUndo) {
         _scaffold.hideCurrentSnackBar();
         _scaffold.showSnackBar(SnackBar(
           backgroundColor: Theme.of(context).buttonColor,
-          content: Row(
-            children: [
-              Text("Card exchange undone.", 
+          content: Row(children: [
+            Text("Card exchange undone.",
                 style: TextStyle(
-                  fontWeight: FontWeight.bold, 
-                  color: Theme.of(context).accentColor
-                )
-              ),
-            ]
-          ),
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).accentColor)),
+          ]),
           duration: const Duration(seconds: 2),
         ));
-      }
-      else
-      {
+      } else {
         _scaffold.hideCurrentSnackBar();
         _scaffold.showSnackBar(SnackBar(
           backgroundColor: Theme.of(context).buttonColor,
           content: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                flex: 0,
-                child: Text("${state.initialCard.name}",
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                    flex: 0,
+                    child: Text("${state.initialCard.name}",
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).accentColor,
+                            decoration: TextDecoration.underline))),
+                Expanded(
+                  child: Icon(
+                    FontAwesomeIcons.longArrowAltRight,
+                    size: 18,
                     color: Theme.of(context).accentColor,
-                    decoration: TextDecoration.underline
-                  )
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Text("${state.swappedCard.name}",
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).accentColor,
+                          decoration: TextDecoration.underline)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: GestureDetector(
+                    child: Icon(FontAwesomeIcons.undo,
+                        size: 12, color: Theme.of(context).accentColor),
+                    onTap: () {
+                      _undoSwap();
+                    },
+                  ),
                 )
-              ),
-              Expanded(
-                child: Icon(FontAwesomeIcons.longArrowAltRight,
-                  size: 18,
-                  color: Theme.of(context).accentColor,
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Text("${state.swappedCard.name}",
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold, 
-                    color: Theme.of(context).accentColor, 
-                    decoration: TextDecoration.underline
-                  )
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: GestureDetector(
-                  child: Icon(FontAwesomeIcons.undo, size: 12, color: Theme.of(context).accentColor),
-                  onTap: () { _undoSwap(); },
-                ),
-              )
-            ]
-          ),
+              ]),
           duration: const Duration(seconds: 2),
         ));
       }
@@ -113,7 +107,10 @@ class KingdomPageState extends State<KingdomPage> {
 
   void _newShuffle() {
     List<int> setIds = _sets.map((si) => si.id).toList();
-    kingdomBloc.drawNewKingdom(shuffleSize: _cardsToShuffle, autoBlacklist: _autoBlacklist, setIds: setIds);
+    kingdomBloc.drawNewKingdom(
+        shuffleSize: _cardsToShuffle,
+        autoBlacklist: _autoBlacklist,
+        setIds: setIds);
   }
 
   void _onSetInitialize(SetsBlocState setsState) {
@@ -127,12 +124,14 @@ class KingdomPageState extends State<KingdomPage> {
 
   void _showDialog() {
     showDialog<int>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return SortDialog(_sortType?.index ?? KingdomSortType.CardNameAscending, KingdomSortTypeNames, 400);
-      }
-    ).then((sortType) {
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SortDialog(
+              _sortType?.index ?? KingdomSortType.CardNameAscending,
+              KingdomSortTypeNames,
+              400);
+        }).then((sortType) {
       if (sortType != null) {
         kingdomBloc.sortKingdom(KingdomSortType.values[sortType]);
       }
@@ -150,128 +149,144 @@ class KingdomPageState extends State<KingdomPage> {
   }
 
   @override
-  Widget build (BuildContext ctxt) {
+  Widget build(BuildContext ctxt) {
     SettingsBloc appBloc = AppSettingsProvider.of(context);
-    appBloc.appStateStream.where((s) => s.autoBlacklist !=_autoBlacklist || s.cardsToShuffle !=_cardsToShuffle).listen(_onAppStateChange);
+    appBloc.appStateStream
+        .where((s) =>
+            s.autoBlacklist != _autoBlacklist ||
+            s.cardsToShuffle != _cardsToShuffle)
+        .listen(_onAppStateChange);
     appBloc.initialize();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Kingdom Page"),
-        actions: <Widget>[
-          StreamBuilder<SettingsState>(
-            stream: appBloc.appStateStream,
-            builder: (BuildContext context, AsyncSnapshot<SettingsState> snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return const CircularProgressIndicator();
-                default:
-                  return IconButton(
-                    icon: Icon(FontAwesomeIcons.dice),
-                    onPressed: _newShuffle,
-                  );
-              }
-            }
-          ),
-          StreamBuilder<KingdomState>(
-            stream: kingdomBloc.kingdomStream,
-            builder: (BuildContext context, AsyncSnapshot<KingdomState> snapshot) {
-              switch (snapshot.connectionState) {
-                default:
-                  if (snapshot.hasError)
-                    return Text('Error: ${snapshot.error}');
-                  else if (!snapshot.hasData || snapshot.data.cards.length == 0)
-                    return IconButton(
-                      icon: Icon(FontAwesomeIcons.sort),
-                      onPressed: null,
-                    );
-                  else
-                    return IconButton(
-                      icon: Icon(FontAwesomeIcons.sort),
-                      onPressed: _showDialog,
-                    );
-              }
-            }
-          ),
-        ],
-      ),
-      drawer: new MyDrawer(),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: StreamBuilder<KingdomState>(
+        appBar: AppBar(
+          title: Text("Kingdom Page"),
+          actions: <Widget>[
+            StreamBuilder<SettingsState>(
+                stream: appBloc.appStateStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<SettingsState> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const CircularProgressIndicator();
+                    default:
+                      return IconButton(
+                        icon: Icon(FontAwesomeIcons.dice),
+                        onPressed: _newShuffle,
+                      );
+                  }
+                }),
+            StreamBuilder<KingdomState>(
+                stream: kingdomBloc.kingdomStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<KingdomState> snapshot) {
+                  switch (snapshot.connectionState) {
+                    default:
+                      if (snapshot.hasError)
+                        return Text('Error: ${snapshot.error}');
+                      else if (!snapshot.hasData ||
+                          snapshot.data.cards.length == 0)
+                        return IconButton(
+                          icon: Icon(FontAwesomeIcons.sort),
+                          onPressed: null,
+                        );
+                      else
+                        return IconButton(
+                          icon: Icon(FontAwesomeIcons.sort),
+                          onPressed: _showDialog,
+                        );
+                  }
+                }),
+          ],
+        ),
+        drawer: new MyDrawer(),
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+                    Widget>[
+          Expanded(
+              child: StreamBuilder<KingdomState>(
                   stream: kingdomBloc.kingdomStream,
-                  builder: (BuildContext context, AsyncSnapshot<KingdomState> snapshot) {
+                  builder: (BuildContext context,
+                      AsyncSnapshot<KingdomState> snapshot) {
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
                         return const CircularProgressIndicator();
                       default:
                         if (snapshot.hasError)
                           return Text('Error: ${snapshot.error}');
-                        else if (!snapshot.hasData || snapshot.data.totalCards == 0)
+                        else if (!snapshot.hasData ||
+                            snapshot.data.totalCards == 0)
                           return Align(
-                            alignment: Alignment.center,
-                            child: const Text(
-                              "Tap the dice to\ngenerate a kingdom.", 
-                              style: TextStyle(fontSize: 36),
-                              textAlign: TextAlign.center,
-                            )
-                          );
+                              alignment: Alignment.center,
+                              child: const Text(
+                                "Tap the dice to\ngenerate a kingdom.",
+                                style: TextStyle(fontSize: 36),
+                                textAlign: TextAlign.center,
+                              ));
                         else
-                          return ListView.builder
-                          (
-                            itemCount: snapshot.data.totalCards,
-                            itemBuilder: (BuildContext ctxt, int index) {
-                              if (index < snapshot.data.numberOfKingdomCards) {
-                                return Dismissible(
-                                  background: Container(
-                                    color: Theme.of(context).accentColor,
-                                    padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
-                                    child: Align(
-                                      alignment: Alignment.centerRight,
-                                      child: new Icon(FontAwesomeIcons.random, color: Theme.of(context).colorScheme.onSecondary),
-                                    )
-                                  ),
-                                  key: Key(snapshot.data.cards[index].id.toString()),
-                                  onDismissed: (d) {
-                                    _scaffold = Scaffold.of(context);
-                                    _swapCard(snapshot.data.cards[index]);
-                                  },
-                                  direction: DismissDirection.endToStart,
-                                  child: KingdomCardItem(
-                                    card: snapshot.data.cards[index],
-                                    isBroughtCard: false,
-                                    topBorder: false
-                                  )
-                                );
-                              } else if (index >= snapshot.data.numberOfKingdomCards && index < (snapshot.data.numberOfKingdomCards + snapshot.data.numberOfBroughtCards)) {
-                                return KingdomCardItem(
-                                  card: snapshot.data.broughtCards[index - snapshot.data.numberOfKingdomCards],
-                                  isBroughtCard: true,
-                                  topBorder: index == snapshot.data.numberOfKingdomCards,
-                                );
-                              } else {
-                                // Events, Landmarks, and Projects
-                                return KingdomCardItem(
-                                  card: snapshot.data.eventsLandmarksProjects[index - snapshot.data.numberOfKingdomCards - snapshot.data.numberOfBroughtCards],
-                                  isEventProjectOrLandmark: true,
-                                  topBorder: index == (snapshot.data.numberOfKingdomCards + snapshot.data.numberOfBroughtCards),
-                                );
-                              }
-                            }
-                          );
+                          return ListView.builder(
+                              itemCount: snapshot.data.totalCards,
+                              itemBuilder: (BuildContext ctxt, int index) {
+                                if (index <
+                                    snapshot.data.numberOfKingdomCards) {
+                                  return Dismissible(
+                                      background: Container(
+                                          color: Theme.of(context).accentColor,
+                                          padding: const EdgeInsets.fromLTRB(
+                                              16.0, 0, 16.0, 0),
+                                          child: Align(
+                                            alignment: Alignment.centerRight,
+                                            child: new Icon(
+                                                FontAwesomeIcons.random,
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onSecondary),
+                                          )),
+                                      key: Key(snapshot.data.cards[index].id
+                                          .toString()),
+                                      onDismissed: (d) {
+                                        _scaffold = Scaffold.of(context);
+                                        _swapCard(snapshot.data.cards[index]);
+                                      },
+                                      direction: DismissDirection.endToStart,
+                                      child: KingdomCardItem(
+                                          card: snapshot.data.cards[index],
+                                          isBroughtCard: false,
+                                          topBorder: false));
+                                } else if (index >=
+                                        snapshot.data.numberOfKingdomCards &&
+                                    index <
+                                        (snapshot.data.numberOfKingdomCards +
+                                            snapshot
+                                                .data.numberOfBroughtCards)) {
+                                  return KingdomCardItem(
+                                    card: snapshot.data.broughtCards[index -
+                                        snapshot.data.numberOfKingdomCards],
+                                    isBroughtCard: true,
+                                    topBorder: index ==
+                                        snapshot.data.numberOfKingdomCards,
+                                  );
+                                } else {
+                                  // Events, Landmarks, and Projects
+                                  return KingdomCardItem(
+                                    card: snapshot.data.eventsLandmarksProjects[
+                                        index -
+                                            snapshot.data.numberOfKingdomCards -
+                                            snapshot.data.numberOfBroughtCards],
+                                    isEventProjectOrLandmark: true,
+                                    topBorder: index ==
+                                        (snapshot.data.numberOfKingdomCards +
+                                            snapshot.data.numberOfBroughtCards),
+                                  );
+                                }
+                              });
                     }
-                  }
-                )
-              )
-            ]
-          )
-        )
-      );
+                  }))
+        ])));
   }
 }
-class KingdomPage extends StatefulWidget {    
+
+class KingdomPage extends StatefulWidget {
   KingdomPage({Key key, this.title}) : super(key: key);
 
   final String title;
